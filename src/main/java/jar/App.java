99ccb8280f84ce9683176b361e1c3f767db92b6e
Aarnavreddy <sowjireddy@gmail.com>
2017-07-23 15:08:39 +0000

    to deploying

--- src/main/java/jar/App.java
+++ src/main/java/jar/App.java
@@ -1,19 +1,13 @@
 package jar;
 
-import org.gmr.web.multipart.GMultipartResolver;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.servlet.DispatcherServlet;
-
-@SpringBootApplication
-public class App {
-
-    @Bean(name = DispatcherServlet.MULTIPART_RESOLVER_BEAN_NAME)
-    MultipartResolver multipartResolver(@Value("${multipart.maxFileSize:1048576}") int maxUploadSize) {
-        GMultipartResolver multipartResolver = new GMultipartResolver();
-        multipartResolver.setMaxUploadSize(maxUploadSize);
-        return multipartResolver;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }
